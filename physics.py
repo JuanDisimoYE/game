3c4334efa3e7967c1b30fbc1c9ab4ed230511dd3
Julian Tölke <julian.toelki@web.de>
2025-12-18 20:50:47 +0100

    fixed speed in x direction

--- physics.py
+++ physics.py
@@ -2,6 +2,7 @@ import math
 import pygame
 from char import timer
 import os, pathlib
+import copy
 
 
 class circle:
@@ -29,21 +30,20 @@ class circle:
         return (default_position[0] - self.get_default_position()[0], default_position[1] - self.get_default_position()[1])
     
     def set_center(self, center:tuple) -> None:
-        self.prev_center = list(self.center_buffer)
-        self.center_buffer = list(self.center)
-        self.center = list(center)
+        self.prev_center   = copy.deepcopy(list(self.center_buffer))
+        self.center_buffer = copy.deepcopy(list(self.center))
+        self.center        = copy.deepcopy(list(center))
 
     def get_direction_angle(self) -> int:
         ankathete = self.center[1] - self.prev_center[1]
         gegenkathete = self.prev_center[0] - self.center[0]
-        print(f"{gegenkathete}/{ankathete}")
         return arctan(ankathete, gegenkathete)
 
     def get_center(self) -> tuple[int, int]:
-        return self.center
+        return copy.deepcopy(self.center)
     
     def get_prev_center(self) -> tuple[int, int]:
-        return self.prev_center
+        return copy.deepcopy(self.prev_center)
 
 
     def get_default_position(self):
@@ -54,13 +54,17 @@ class circle:
 def set_offset(overlap, offset):
     return (overlap[0]+offset[0], overlap[1]+offset[1])
 
-def get_normal_angle(center:tuple[int, int], overlap:tuple[int, int]) -> int:
+def get_normal_angle(center:tuple[int, int], overlap:tuple[int, int]):
     gegenkathete = overlap[0] - center[0]
     ankathete = overlap[1] - center[1]
+
     
     return arctan(ankathete, gegenkathete)
 
-def arctan(ankathete:int, gegenkathete:int) -> int:
+def get_normal_vektor(angle:float, length) -> list:
+    return [length*math.sin(angle), length*math.sin(angle)]
+
+def arctan(ankathete:int, gegenkathete:int):
     if ankathete == 0:
         angle = math.pi/2
     else:
@@ -70,6 +74,13 @@ def arctan(ankathete:int, gegenkathete:int) -> int:
 
 def get_speed(speed:list[int,int]):
     return math.sqrt( (speed[0]*speed[0]) + (speed[1]*speed[1]) )
+
+def get_distance(point_1:list[int,int], point_2:list[int,int]):
+    return math.sqrt( ( (point_1[0]-point_2[0])**2 ) + (point_1[1]-point_2[1])**2 )
+
+def get_dot_product(vektor_1:list[int,int], vektor_2:list[int,int]) -> float:
+    return (vektor_1[0]*vektor_2[0]) + (vektor_1[1]*vektor_2[1])
+
 
 
 def main():
@@ -92,9 +103,10 @@ def main():
 
     start_position = None
     speed = [0,0]
-    tmp_cooldown = False
 
     _overlap = None
+
+    frames_per_second = 60
 
     while running:
         for event in pygame.event.get(): 
@@ -102,7 +114,7 @@ def main():
                 running = False
 
         if fps_timer.is_timer_done():
-            fps_timer.set_timer(1/60)
+            fps_timer.set_timer(1/frames_per_second)
 
             screen.fill((0,255,0))
             screen.blit(circle_big.get_image(), (0, 0))
@@ -127,45 +139,36 @@ def main():
                     start_position = pygame.mouse.get_pos()
                     circle_small.set_center(pygame.mouse.get_pos())
 
-                acceleration = 10
-                dt = 1/60
+                acceleration = 100
+                dt = 1/frames_per_second
 
-                prev = circle_small.get_center()
+                    
                 position = circle_small.get_center()
-                speed[1] += acceleration*dt
-                position[1] += speed[1]*dt
-                position[0] += speed[0]
-
-                print(f"{speed[0]}/{speed[1]}")
+                speed[1] = speed[1] + acceleration*dt
+                position[1] = position[1] + speed[1]*dt
+                position[0] = position[0] + speed[0]*dt
 
                 circle_small.set_center(position)
 
                 overlap = circle_small.is_overlap(circle_big.get_mask(), circle_big.get_default_position())
 
-                if overlap and not tmp_cooldown:
+                if overlap:
                     _overlap = set_offset(overlap, circle_small.get_default_position())
                     normal_angle = get_normal_angle(circle_small.get_center(), _overlap)
-                    # an = circle_small.get_prev_center()[0] - circle_small.get_center()[0]
-                    # gegen = circle_small.get_center()[1] - circle_small.get_prev_center()[1]
-                    # normal_angle = arctan(an, gegen)
-
-                    comming_in_angle = circle_small.get_direction_angle()
-
-                    angle_difference = comming_in_angle + normal_angle
-
-                    outgoing_angle = normal_angle + angle_difference
-                    
                     speed_0 = get_speed(speed)
-                    print(f"Vx, Vy, V: {speed[0]}, {speed[1]} {speed_0}")
-                    speed[0] = - speed_0 * math.cos(outgoing_angle)
-                    speed[1] = - speed_0 * math.sin(outgoing_angle)
+                    normal_vektor = get_normal_vektor(normal_angle, speed_0)
 
-                    tmp_cooldown = True
+                    if get_dot_product(speed, normal_vektor) < 0:
+                        comming_in_angle = circle_small.get_direction_angle()
 
-                    # print(normal_angle *57.3)
-                    print(f"comming[normal]going: {comming_in_angle*57.3}[{normal_angle*57.3}]{outgoing_angle*57.3}")
-                else:
-                    tmp_cooldown = False
+                        angle_difference = comming_in_angle + normal_angle
+
+                        outgoing_angle = normal_angle + angle_difference
+
+                        print(f"{comming_in_angle*57.3}/{angle_difference*57.3}/{outgoing_angle*57.3}")
+                        
+                        speed[0] =   speed_0 * math.cos(outgoing_angle)
+                        speed[1] = - speed_0 * math.sin(outgoing_angle)
                 
 
 
